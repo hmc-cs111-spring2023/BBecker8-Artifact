e7280478178bd57ebafae1374730bf370177e9cf
Ben Wiedermann <bwiedermann@users.noreply.github.com>
2023-04-12 03:56:20 +0000

    rolling back changes to test google sheets

--- my-dsl/app/src/main/java/my/dsl/App.java
+++ my-dsl/app/src/main/java/my/dsl/App.java
@@ -15,14 +15,7 @@ public class App extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        // Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " +
-        // javaVersion + ".");
-        Label l = new Label(Sheet.class.toString());
-        try {
-            Sheet.createSpreadsheet("test");
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
